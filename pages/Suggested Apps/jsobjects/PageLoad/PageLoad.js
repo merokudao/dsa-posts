387cbff484d69c25ac3d681ea8e3ea61702f126b
saurabh.suthar <saurabh.suthar@bitpack.me>
2024-01-22 11:24:21 +0000

    apiUrl toggle added

--- pages/Suggested Apps/jsobjects/PageLoad/PageLoad.js
+++ pages/Suggested Apps/jsobjects/PageLoad/PageLoad.js
@@ -1,8 +1,4 @@
 export default {
-	pageLoad () {
-		clearStore()
-		storeValue("save_to_db_flag", false);
-	},
 	setAPIUrlOnPageLoad(){
 		const prodUrl = "https://api.dappstore.app/";
 		const testApiUrl = "https://test-api.dappstore.app/";
