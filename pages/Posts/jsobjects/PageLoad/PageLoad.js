96ee01a3e907780916482b436232870c9a1afd02
saurabh.suthar <saurabh.suthar@bitpack.me>
2024-01-22 11:55:34 +0000

    Logging active api

--- pages/Posts/jsobjects/PageLoad/PageLoad.js
+++ pages/Posts/jsobjects/PageLoad/PageLoad.js
@@ -2,12 +2,11 @@ export default {
 	pageLoad () {
 		clearStore()
 		storeValue("save_to_db_flag", false);
-	},
-	setAPIUrlOnPageLoad(){
 		const prodUrl = "https://api.dappstore.app/";
 		const testUrl = "https://test-api.dappstore.app/";
-
+		storeValue("apiMode", false);
 		storeValue("prodUrl", prodUrl)
 		storeValue("testUrl", testUrl);
-	}
+		console.log("page load worked");
+	},
 }
